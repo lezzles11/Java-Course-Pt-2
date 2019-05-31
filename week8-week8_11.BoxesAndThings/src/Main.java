46c1114fdcec76fa2263ecb3e96319f41283b6e8
lezzles11 <lesleyc@bu.edu>
2019-05-31 18:05:51 -0400

    Update Week 8

---
+++ week8-week8_11.BoxesAndThings/src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lesleycheung
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Box box = new Box(5);
+        Box bb2 = new Box(1);
+
+
+        System.out.println(box);
+    }
+}
